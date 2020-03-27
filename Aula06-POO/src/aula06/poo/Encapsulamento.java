fa603d8f29e5f47d2b0db40e41a1c98b64538691
Thifany Gomes <51092564+thifanyholanda@users.noreply.github.com>
2020-03-27 19:01:14 -0300

    Aula 06 - Encapsulamento

---
+++ Aula06-POO/src/aula06/poo/Encapsulamento.java
@@ -0,0 +1,13 @@
+package aula06.poo;
+public class Encapsulamento {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        
+    }
+    
+}
